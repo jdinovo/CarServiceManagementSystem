156602bc51fa4f33a98dba37a30acfd860d483e5
James DiNovo <james.dinovo01@stclairconnect.ca>
2018-10-29 16:48:06 -0400

    menuBar, tabs added #3 #4 #8 - Added menuBar which contains File, Edit, and Statistics menus - each menu item within these menus opens a tab - Added NewWorkOrderTab - Added StatisticsTab - Tabs are singletons - #1 added .jar required for Database connection

--- src/panes/MainMenuPane.java
+++ src/panes/MainMenuPane.java
@@ -3,13 +3,16 @@ package panes;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
+import tabs.NewWorkOrderTab;
+import tabs.StatisticsTab;
 
 /**
  * 
  * @author James DiNovo
  * @date 17.10.2018
- * @version 1.0
+ * @version 1.2
  * 
  * MainMenuPane is the pane that launches from MainMenuScene
  *
@@ -20,32 +23,66 @@ public class MainMenuPane extends BorderPane {
 	 * 
 	 * @author James DiNovo
 	 * @date 17.10.2018
-	 * @version 1.0
+	 * @version 1.2
 	 * 
 	 * Constructor for MainMenuPane contains GUI
 	 * GUI contains a menubar that opens tabs
 	 *
 	 */
 	public MainMenuPane() {
+		//create menuBar
 		MenuBar menu = new MenuBar();
 		
+		//create menu choices
 		Menu fileMenu = new Menu("File");
 		Menu editMenu = new Menu("Edit");
 		Menu statsMenu = new Menu("Statistics");
 		
-		MenuItem newInv = new MenuItem("New Invoice");
-		MenuItem loadInv = new MenuItem("Load Invoice");
-		MenuItem editInv = new MenuItem("Edit Invoice");
-		MenuItem serviceTables = new MenuItem("Service History");
-		MenuItem serviceCharts = new MenuItem("Service Charts");
+		//creating tabPane
+		TabPane tabPane = new TabPane();
 		
-		fileMenu.getItems().addAll(newInv, loadInv);
-		editMenu.getItems().addAll(editInv);
+		//create tabs
+		NewWorkOrderTab newWorkTab = NewWorkOrderTab.getInstance();
+		StatisticsTab statsTab = StatisticsTab.getInstance();
+		
+		//creating menu items and adding event listeners to them
+		MenuItem newWork = new MenuItem("New Invoice");
+		newWork.setOnAction(e-> {
+			//if tab is not already open
+			if(!tabPane.getTabs().contains(newWorkTab)) {
+				tabPane.getTabs().add(newWorkTab);
+			}
+		});
+		MenuItem loadWork = new MenuItem("Load Invoice");
+		loadWork.setOnAction(e-> {
+			
+		});
+		MenuItem editWork = new MenuItem("Edit Invoice");
+		editWork.setOnAction(e-> {
+			
+		});
+		MenuItem serviceTables = new MenuItem("Service History");
+		serviceTables.setOnAction(e-> {
+			if(!tabPane.getTabs().contains(statsTab)) {
+				tabPane.getTabs().add(statsTab);
+			}
+		});
+		MenuItem serviceCharts = new MenuItem("Service Charts");
+		serviceCharts.setOnAction(e-> {
+			
+		});
+		
+		//adding menu items to menus
+		fileMenu.getItems().addAll(newWork, loadWork);
+		editMenu.getItems().addAll(editWork);
 		statsMenu.getItems().addAll(serviceTables, serviceCharts);
 		
+		//adding menus to menubar
 		menu.getMenus().addAll(fileMenu, editMenu, statsMenu);
 		
+		//adding menubar and tabpane to pane
 		this.setTop(menu);
+		this.setCenter(tabPane);
 		
 	}
 
---
+++ src/tabs/StatisticsTab.java
@@ -0,0 +1,47 @@
+package tabs;
+
+import javafx.scene.control.Tab;
+
+/**
+ * 
+ * @author James DiNovo
+ * @date 29.10.2018
+ * @version 1.0
+ * 
+ * Statistics is a tab that contains the GUI for viewing service statistics
+ * It is a singleton class
+ *
+ */
+public class StatisticsTab extends Tab {
+	
+	private static StatisticsTab tab;
+
+	/**
+	 * 
+	 * @author James DiNovo
+	 * @date 29.10.2018
+	 * @version 1.0
+	 * 
+	 * Statistics private constructor
+	 *
+	 */
+	private StatisticsTab() {
+		this.setText("Statistics");
+	}
+	
+	/**
+	 * 
+	 * @author James DiNovo
+	 * @date 29.10.2018
+	 * @version 1.0
+	 * 
+	 * Statistics getInstance method
+	 *
+	 */
+	public static StatisticsTab getInstance() {
+		if(tab == null) {
+			tab = new StatisticsTab();
+		}
+		return tab;
+	}
+}
