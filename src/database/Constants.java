bfe9c29c9315cf0fd38ab4c8eec211397532473c
James DiNovo <james.dinovo01@stclairconnect.ca>
2018-10-29 16:01:06 -0400

    Added menuBar #3

---
+++ src/database/Constants.java
@@ -0,0 +1,5 @@
+package database;
+
+public class Constants {
+
+}
--- src/panes/MainMenuPane.java
+++ src/panes/MainMenuPane.java
@@ -1,5 +1,8 @@
 package panes;
 
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -20,9 +23,29 @@ public class MainMenuPane extends BorderPane {
 	 * @version 1.0
 	 * 
 	 * Constructor for MainMenuPane contains GUI
+	 * GUI contains a menubar that opens tabs
 	 *
 	 */
 	public MainMenuPane() {
+		MenuBar menu = new MenuBar();
+		
+		Menu fileMenu = new Menu("File");
+		Menu editMenu = new Menu("Edit");
+		Menu statsMenu = new Menu("Statistics");
+		
+		MenuItem newInv = new MenuItem("New Invoice");
+		MenuItem loadInv = new MenuItem("Load Invoice");
+		MenuItem editInv = new MenuItem("Edit Invoice");
+		MenuItem serviceTables = new MenuItem("Service History");
+		MenuItem serviceCharts = new MenuItem("Service Charts");
+		
+		fileMenu.getItems().addAll(newInv, loadInv);
+		editMenu.getItems().addAll(editInv);
+		statsMenu.getItems().addAll(serviceTables, serviceCharts);
+		
+		menu.getMenus().addAll(fileMenu, editMenu, statsMenu);
+		
+		this.setTop(menu);
 		
 	}
 
