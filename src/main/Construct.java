0ef798b92294b6f265f510546d0a4659f624f3ae
raju7274 <rs44951@wdmcs.org>
2024-02-25 12:00:28 -0600

    fix: changed screen height

--- src/main/Construct.java
+++ src/main/Construct.java
@@ -10,7 +10,7 @@ package main;
  */
 public class Construct {
 	
-	public static final int SCREEN_HEIGHT = 768;
+	public static final int SCREEN_HEIGHT = 800;
 	public static final int SCREEN_WIDTH = 1024;
 
 }
